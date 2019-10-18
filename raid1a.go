5ae98c7048335b7b06271fc33fe7ee419c748904
Miras <sektor.miras@gmail.com>
2019-10-18 18:51:23 +0600

    first task

--- raid1a.go
+++ raid1a.go
@@ -1,27 +1,9 @@
-package raid1a
+package main
 
 import "github.com/01-edu/z01"
 
 func Raid1a(x, y int) {
-	for i := 1; i <= x; i++ {
-		if i == 1 || i == x {
-			z01.PrintRune('o')
-		} else {
-			z01.PrintRune('-')
-		}
-	}
-	z01.PrintRune(10)
-	for i := 2; i <= y-1; i++ {
-		for j := 1; j <= x; j++ {
-			if j == 1 || j == x {
-				z01.PrintRune(124)
-			} else {
-				z01.PrintRune(' ')
-			}
-		}
-		z01.PrintRune(10)
-	}
-	if y != 1 {
+	if (x >= 0 && y >= 0) {
 		for i := 1; i <= x; i++ {
 			if i == 1 || i == x {
 				z01.PrintRune('o')
@@ -30,5 +12,31 @@ func Raid1a(x, y int) {
 			}
 		}
 		z01.PrintRune(10)
+		for i := 2; i <= y-1; i++ {
+			for j := 1; j <= x; j++ {
+				if j == 1 || j == x {
+					z01.PrintRune(124)
+				} else {
+					z01.PrintRune(' ')
+				}
+			}
+			z01.PrintRune(10)
+		}
+		if y != 1 {
+			for i := 1; i <= x; i++ {
+				if i == 1 || i == x {
+					z01.PrintRune('o')
+				} else {
+					z01.PrintRune('-')
+				}
+			}
+			z01.PrintRune(10)
+		}
+	} else {
+		z01.PrintRune(0)
 	}
+}
+
+func main() {
+	Raid1a(-1, 6)
 }
