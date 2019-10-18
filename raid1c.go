317868c6152c9af4a02dcc4558864bdfd6ff43bd
Miras <sektor.miras@gmail.com>
2019-10-18 19:22:18 +0600

    3 task

---
+++ raid1c.go
@@ -0,0 +1,40 @@
+package raid1c
+
+import "github.com/01-edu/z01"
+
+func Raid1c(x, y int) {
+	if x > 0 && y > 0 {
+		for i := 1; i <= x; i++ {
+			if i == 1 {
+				z01.PrintRune('A')
+			} else if i == x {
+				z01.PrintRune('A')
+			} else {
+				z01.PrintRune('B')
+			}
+		}
+		z01.PrintRune(10)
+		for i := 2; i <= y-1; i++ {
+			for j := 1; j <= x; j++ {
+				if j == 1 || j == x {
+					z01.PrintRune('B')
+				} else {
+					z01.PrintRune(' ')
+				}
+			}
+			z01.PrintRune(10)
+		}
+		if y != 1 {
+			for i := 1; i <= x; i++ {
+				if i == 1 {
+					z01.PrintRune('C')
+				} else if i == x {
+					z01.PrintRune('C')
+				} else {
+					z01.PrintRune('B')
+				}
+			}
+			z01.PrintRune(10)
+		}
+	}
+}
